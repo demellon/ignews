e9ee0a71ea26938fb55e584043edd3223d9b3466
Your Name <you@example.com>
2022-06-29 22:19:48 +0100

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import { FiX } from 'react-icons/fi';
+import { FaGithub } from 'react-icons/fa';
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+    const { data: session } = useSession()
+    return session ? (
+        <button
+            type="button"
+            className={styles.SignInButton}
+            onClick={() => signOut()}
+        >
+            <FaGithub color='#04d361' />
+            {session.user.name}
+            <FiX color='#737380' className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button
+            type="button"
+            className={styles.SignInButton}
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color='#eba417' />
+            Sign in with Github
+        </button>
+    );
+}
